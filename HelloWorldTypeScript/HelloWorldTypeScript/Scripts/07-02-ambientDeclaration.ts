7dd3d68bb166e28a0f7ccb0037bb30f1bc3c7730
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2018-02-14 17:31:50 +0200

    Module_2_Typing_variables_and_functions

--- HelloWorldTypeScript/HelloWorldTypeScript/Scripts/07-02-ambientDeclaration.ts
+++ HelloWorldTypeScript/HelloWorldTypeScript/Scripts/07-02-ambientDeclaration.ts
@@ -1,17 +1,17 @@
-﻿/// <reference path="typings/knockout-2.2.d.ts" />
-declare var ko: KnockoutStatic
+﻿///// <reference path="typings/knockout-2.2.d.ts" />
+//declare var ko: KnockoutStatic
 
-module ambientDeclaration {
+//module ambientDeclaration {
 
-    var name = ko.observable("mohamed magdy");
-    var id = ko.observable(1);
+//    var name = ko.observable("mohamed magdy");
+//    var id = ko.observable(1);
 
-    var guy = {
-        id: id,
-        fullName: name
-    }
+//    var guy = {
+//        id: id,
+//        fullName: name
+//    }
 
-    var value: string = guy.fullName();
-    console.log(value);
-}
+//    var value: string = guy.fullName();
+//    console.log(value);
+//}
 
